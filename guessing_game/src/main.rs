3efcecae609b1b7e3b6a4014e1e0d33fe473a6de
Phil Ganem <philipganem@gmail.com>
2021-04-27 05:14:24 -0400

    Added comments to guessing game main

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,32 @@
+// This is a use statement, works much like an import statement.
+// std is the standard library, io is the input/output library 
+use std::io;
+
+//Main dec
 fn main() {
-    println!("Hello, world!");
+    
+    //Standard println - prints a string to CLI with a newline char
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    // Let declares a var
+    // mut makes a var mutable - vars are immutable by default in Rust
+    // String::new() declares a new str - does not take a str, only declares  
+    // the "::" in String::new() shows association between the str type and new func 
+    let mut guess = String::new(); 
+
+    //Call to stdin func from io library declared at top of the file
+    //stdin returns an instance of "std::io::Stdin" which represents a handle to the standard input for your terminal
+    io::stdin()
+        //Calls the read_line method on the stdin handle
+        //passes in one arg (guess), and writes the input from CLI to that var
+        //the "&" indicates that its a reference to the guess var - references are immutable by default, mut allows it to be mutated
+        .read_line(&mut guess)
+        //results from read_line are enums - either 'Ok' or 'Err'
+        //if an 'Err', or error, is returned, the expected method is hit
+        .expect("Failed to read line");
+
+    //Normal printline, but includes formating to display the 'guess' var
+    //'{}'s are placeholders that show that you wish to display a var that you passed in as an arg - you can do this with many values
+    println!("You guessed: {}", guess);
 }
