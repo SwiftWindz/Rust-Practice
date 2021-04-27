c5c4e2605ab39a422d5e74eb7dc1aac409c34190
Phil Ganem <philipganem@gmail.com>
2021-04-27 06:43:03 -0400

    vars dir

---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\Phil\personal_projects\RustTest\variables\target\debug\variables.exe: C:\Users\Phil\personal_projects\RustTest\variables\src\main.rs
---
+++ variables/target/rls/debug/deps/variables-ff2fc31f99a95ec8.d
@@ -0,0 +1,5 @@
+c:\Users\Phil\personal_projects\RustTest\variables\target\rls\debug\deps\variables-ff2fc31f99a95ec8.rmeta: src\main.rs
+
+c:\Users\Phil\personal_projects\RustTest\variables\target\rls\debug\deps\variables-ff2fc31f99a95ec8.d: src\main.rs
+
+src\main.rs:
