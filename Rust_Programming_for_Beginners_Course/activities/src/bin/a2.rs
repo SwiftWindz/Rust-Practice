5bf2e7d97a98177d3f3c7dbae16e11ace6e8079e
Phil Ganem <philipganem@gmail.com>
2021-05-28 15:38:57 -0400

    A2 complete

--- Rust_Programming_for_Beginners_Course/activities/src/bin/a2.rs
+++ Rust_Programming_for_Beginners_Course/activities/src/bin/a2.rs
@@ -8,4 +8,16 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+//Adds two given numbers together
+fn addition(a: i32, b: i32) -> i32{
+    return a + b;
+}
+
+//Prints a 32bit int
+fn num_printer(a: i32){
+    println!("{:?}", a);
+}
+
+fn main() {
+    num_printer(addition(10, 10));
+}
