d8c24662851ab5c540804646556a206f001ad191
Phil Ganem <philipganem@gmail.com>
2021-10-09 17:40:06 -0400

    Completed activities 10 and 11

--- Rust_Programming_for_Beginners_Course/activities/src/bin/a10.rs
+++ Rust_Programming_for_Beginners_Course/activities/src/bin/a10.rs
@@ -12,4 +12,27 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+
+fn var_message(foo: bool){
+    // Match statements foo, a bool, to check whether its true or false
+    match foo {
+        true => println!("It's too big"),
+        false => println!("It's too small"),
+    }
+
+}
+
+
+fn main() {
+    let num = 111;
+    let result;
+
+    if num > 100{
+        result = true; 
+    } else {
+        result = false;
+    }
+
+    var_message(result);
+
+}
--- Rust_Programming_for_Beginners_Course/activities/src/bin/a11.rs
+++ Rust_Programming_for_Beginners_Course/activities/src/bin/a11.rs
@@ -9,4 +9,30 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct GroceryItem{
+    //struct for a grocery item
+    quantity: i32,
+    id_number: i32,
+    
+}
+
+fn display_quantity(grocery_item: &GroceryItem){
+    //borrows a grocery item var and displays the quantity
+    println!("Quantity = {:?}", grocery_item.quantity)
+}
+
+fn display_id_number(grocery_item: &GroceryItem){
+    //borrows a grocery item var and displays the id_number
+    println!("ID Number = {:?}", grocery_item.id_number)
+}
+
+fn main() {
+    let item = GroceryItem{
+        quantity: 5,
+        id_number: 32,
+    };
+
+    //Uses '&' which borrows a piece of data and returns it
+    display_id_number(&item);
+    display_quantity(&item)
+}
