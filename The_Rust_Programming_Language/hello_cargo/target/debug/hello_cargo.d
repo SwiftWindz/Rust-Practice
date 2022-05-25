3c9b98d738cce671730b618933f80beeebc39295
Phil Ganem <philipganem@gmail.com>
2022-05-24 23:31:01 -0400

    Chapter 1

---
+++ The_Rust_Programming_Language/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Phil\Projects\Rust-Practice\The_Rust_Programming_Language\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Phil\Projects\Rust-Practice\The_Rust_Programming_Language\hello_cargo\src\main.rs
